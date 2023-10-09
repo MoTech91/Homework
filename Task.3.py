b0887c9ad2667ad22382f35ebdf6a50c99b9d95f
Abdulmuhsen Alsayegh <macmuhsen@me.com>
2023-10-09 13:05:20 +0300

    added the latest task solution

--- Task.3.py
+++ Task.3.py
@@ -49,6 +49,7 @@
 # print(f'The converted value is {result} in {unit}.')
 
 #>>>>>>>>>>>Unsing OOP>>>>>>>>>>>>
+
 # class Converter:
 #     def __init__(self, value, unit):
 #         self.value = value
@@ -78,12 +79,12 @@
 #         else:
 #             return "Invalid conversion type."
 
-# calc = input('Select (T) for temp_calc or (L) for length_calc:  ')
 # unit = input('Enter the appropriate unit from the following (degrees F, degrees C, Inches, cm):  ')
 # value = float(input('Enter input number: '))
 
 # converter = Converter(value, unit)
-# result = converter.convert(calc)
+# conversion_type = input('Select (T) for temperature or (L) for length:  ')
+# result = converter.convert(conversion_type)
 
 # print(f'The converted value is {result} in {unit}.')
 
@@ -119,5 +120,92 @@
 # value = float(input('Enter input number: '))
 
 # result = convert(value, unit, calc)
-
 # print(f'The converted value is {result} in {unit}.')
+
+# # <<<<<<<<<<<<<<Email Slicer<<<<<<<<<<<<<
+
+# import re
+# #promopt user for input
+# email = input("Enter your email: ")
+# #expression matching
+# pattern = r"([\w]+)@([\w]+\.[\w]+)"
+# #pattern matching for username and domain name
+# match = re.match(pattern,email)
+# #extraction
+# username  =  match.group(1)
+# domain_name = match.group(2)
+# #displaying the results
+# print("Username: ", username)
+# print("Domain name: ",domain_name)
+
+
+from forex_python.converter import CurrencyRates
+
+# #Converts a currency from one currency to another
+# def convert_currency(from_currency, to_currency, amount):
+#     c = CurrencyRates()
+#     conversion_rate = c.get_rate(from_currency, to_currency)
+#     converted_amount = amount * conversion_rate
+#     return converted_amount, to_currency
+
+# # Prompting user for input
+# amount = float(input('Enter the amount: '))
+# from_currency  = input("From Currency: ").upper
+# to_currency = input("To Currency: ").upper()
+
+# # Checking for invalid currency codes
+# if from_currency not in CurrencyRates.currencies:
+#     raise ValueError(f"Invalid currency code: {from_currency}")
+# if to_currency not in CurrencyRates.currencies:
+#     raise ValueError(f"Invalid currency code: {to_currency}")
+# result = convert_currency(from_currency, to_currency, amount)
+# print(f"From {from_currency}, To {to_currency}, Amount {amount}")
+# print(f"Converted amount: {converted_amount} {to_currency}")
+from forex_python.converter import CurrencyRates
+
+from forex_python.converter import CurrencyRates
+
+# Define conversion function
+def convert_currency(from_currency, to_currency, amount):
+  """Converts a currency from one currency to another.
+
+  Args:
+    from_currency: The currency code of the source currency.
+    to_currency: The currency code of the target currency.
+    amount: The amount to be converted.
+
+  Returns:
+    A tuple containing the converted amount and the currency code of the target currency, or an error string.
+  """
+  c = CurrencyRates()
+
+  # Checking for invalid currency codes
+  if from_currency not in c.get_rates('USD'):
+    return (f"Invalid currency code: {from_currency}",)
+  if to_currency not in c.get_rates('USD'):
+    return (f"Invalid currency code: {to_currency}",)
+
+  conversion_rate = c.get_rate(from_currency, to_currency)
+  converted_amount = amount * conversion_rate
+  return converted_amount, to_currency
+
+# Prompting user for input
+amount = float(input('Enter the amount: '))
+from_currency = input("From Currency: ").upper()
+to_currency = input("To Currency: ").upper()
+
+# Converting the currency
+result = convert_currency(from_currency, to_currency, amount)
+
+# Check if the conversion was successful
+if len(result) == 1:
+  # An error occurred
+  print(result[0])
+else:
+  # Conversion was successful
+  converted_amount, to_currency = result
+
+  # Printing the result
+  print(f"From {from_currency}, To {to_currency}, Amount {amount}")
+  print(f"Converted amount: {converted_amount} {to_currency}")
+
